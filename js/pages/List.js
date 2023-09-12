0dd35851fe034fc6291e0640d2578ce2aebcb949
simse <55249228+SimseSIM@users.noreply.github.com>
2023-09-12 17:46:02 +0200

    double fixed the double showcase bug i hope

--- js/pages/List.js
+++ js/pages/List.js
@@ -41,7 +41,7 @@ export default {
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <div v-if="level.showcase" class="tabs">
-                        <button class="tab" :class="{selected: toggledShowcase}" @click="toggledShowcase = true">
+                        <button class="tab type-label-lg" :class="{selected: toggledShowcase}" @click="toggledShowcase = true">
                             <span class="type-label-lg">Showcase</span>
                         </button>
                     </div>
