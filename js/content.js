3b68d738ebafec70a22b0830c98f41a1799912ea
simse <55249228+SimseSIM@users.noreply.github.com>
2023-09-22 17:25:38 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -72,7 +72,7 @@ export async function fetchLeaderboard() {
                 completed.push({
                     rank: rank + 1,
                     level: level.name,
-                    score: score(rank + 1, 100, level.percentToQualify),
+                    score: score(100),
                     link: record.link,
                 });
                 return;
