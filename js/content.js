2fa42e6f153aeeb872fb13a76a8a19ca541adf4d
simse <55249228+SimseSIM@users.noreply.github.com>
2023-09-22 17:15:37 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -57,23 +57,6 @@ export async function fetchLeaderboard() {
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
-        };
-        const { verified } = scoreMap[verifier];
-        verified.push({
-            rank: rank + 1,
-            level: level.name,
-            score: score(rank + 1, 100, level.percentToQualify),
-            link: level.verification,
-        });
-
         // Records
         level.records.forEach((record) => {
             const user = Object.keys(scoreMap).find(
